add3405c4e06be7bb316cc9b7191e7fc0a22a216
Naman24363 <naman24363@iiitd.ac.in>
2025-06-15 16:45:54 +0530

    gfooter responsive

--- templates/manage.py
+++ templates/manage.py
@@ -1,1 +1,1 @@
-fix# This file was removed because it was misplaced. Django's manage.py should only be in the backend root directory.
+# This file was removed because it was misplaced. Django's manage.py should only be in the backend root directory.
