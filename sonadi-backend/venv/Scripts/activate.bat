110e80b8fa3beae65f9be1e41245bef0ac54a90f
SonadiTrust <sonadicharitytrust@gmail.com>
2025-07-13 11:54:10 +0530

    new laptop setup

--- sonadi-backend/venv/Scripts/activate.bat
+++ sonadi-backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\chugn\OneDrive\Desktop\sonadi website\sonadi-backend\venv"
+set "VIRTUAL_ENV=D:\sonadi website\sonadi-backend\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
