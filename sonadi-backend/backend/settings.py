a6271b98bba337982d435a22b448feb4397d98da
SonadiTrust <sonadicharitytrust@gmail.com>
2025-07-14 14:44:50 +0530

    mail credentials

--- sonadi-backend/backend/settings.py
+++ sonadi-backend/backend/settings.py
@@ -88,11 +88,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USE_TLS = True          # STARTTLS encryption
+EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sonadicharitytrust@gmail.com'
-EMAIL_HOST_PASSWORD = 'pwppdyfjpfsnytom'   # ← App password (no spaces)
+EMAIL_HOST_PASSWORD = 'tjea oyvv cyso zcyx'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-# Optional—raise errors if email fails during development
-# Set to False in production if you’d rather fail silently
-EMAIL_FAIL_SILENTLY = False
